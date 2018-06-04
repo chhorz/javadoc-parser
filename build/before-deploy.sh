9b49abf67875018741306b35f9cccf780522733d
chhorz <horz.christian@gmail.com>
2018-06-04 22:18:15 +0200

    Another try for path.

--- build/before-deploy.sh
+++ build/before-deploy.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
-    openssl aes-256-cbc -K $encrypted_SOME_key -iv $encrypted_SOME_iv -in build/codesigning.asc.enc -out build/signingkey.asc -d
-    gpg --fast-import build/signingkey.asc
+    openssl aes-256-cbc -K $encrypted_SOME_key -iv $encrypted_SOME_iv -in ./build/codesigning.asc.enc -out ./build/signingkey.asc -d
+    gpg --fast-import ./build/signingkey.asc
 fi
