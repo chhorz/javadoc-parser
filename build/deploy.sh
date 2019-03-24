771a30555a4e00ce7bfb1d84a22dcc60cb516fab
chhorz <horz.christian@gmail.com>
2019-03-24 10:26:19 +0100

    Rework on travis build process

--- build/deploy.sh
+++ build/deploy.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
-    mvn deploy -P sign,build-extras --settings build/settings.xml
+    mvn deploy -P build-extras --settings build/settings.xml
 fi
